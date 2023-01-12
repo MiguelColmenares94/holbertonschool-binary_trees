703df8d7f2c8e1f1e748f1b56ab0f962340ade1f
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-11 16:56:25 -0800

    FIX: correct function for create the new node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -20,10 +20,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent->left)
 	{
-		parent->left->left = createNode(value);
+		parent->left->left = binary_tree_node(parent, value);
 		return (parent->left);
 	}
 
-	parent->left = createNode(value);
+	parent->left = binary_tree_node(parent, value);
 	return (parent->left);
 }
