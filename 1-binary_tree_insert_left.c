4435d836f26ca19f1bff054b889ccd474b047f81
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-11 18:56:56 -0800

    FIX:restructure pointers

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,14 +11,24 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_node(parent, value);
+	binary_tree_t *temp = malloc(sizeof(binary_tree_t));
+
+	if (!temp)
+	{
+		return (NULL);
+	}
 
 	if (parent->left != NULL)
 	{
-		newNode->parent = parent;
-		newNode->left = parent->left;
-		parent->left->parent = newNode;
-		parent->left = newNode;
+		parent->left->parent = temp;
+		parent->left = NULL;
+		newNode = binary_tree_node(parent, value);
+		newNode->left = temp;
+		temp->parent = newNode;
+	}
+	else
+	{
+		parent->left = binary_tree_node(parent, value);
 	}
 
 	return (newNode);
