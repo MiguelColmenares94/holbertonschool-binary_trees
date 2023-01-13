0d53e2834920811557bd775ade965fe47177b5f9
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-12 21:55:07 -0800

    FEAT: measures the height of a binary tree

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,24 @@
+#include "binary_tree.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height.
+ *
+ * Return: height of a binary tree, 0 if NULL.
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t counter;
+
+	if (!tree)
+	{
+		return (0);
+	}
+
+	binary_tree_height(tree->left);
+	binary_tree_height(tree->right);
+	counter++;
+
+	return (counter);
+}
