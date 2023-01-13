d28ecc568040c03c687f1b7c85263d59d08cfaae
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-12 23:30:24 -0800

    FEAT:counts the nodes with at least 1 child in a binary tree

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts the nodes with at least 1
+ * child in a binary tree.
+ * @tree: pointer to the root node of the
+ * tree to count the number of nodes.
+ *
+ * Return: total of nodes with at least 1 child, 0 if fail.
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes;
+
+	if (!tree)
+	{
+		return (0);
+	}
+
+	if (tree->left || tree->right)
+	{
+		return (1);
+	}
+
+	nodes = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+
+	return (nodes);
+}
