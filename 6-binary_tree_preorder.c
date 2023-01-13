d522d4f2ce729e9852284ec6d17c5d587330b837
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-12 21:39:32 -0800

    FEAT:add validation in case func is NULL

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -13,12 +13,14 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree)
+	if (!tree || !func)
 	{
 		return;
 	}
 
-	binary_tree_preorder(tree->left, (*func)(tree->n));
-	binary_tree_preorder(tree->right, (*func)(tree->n));
+	func(tree->n);
+
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
 
