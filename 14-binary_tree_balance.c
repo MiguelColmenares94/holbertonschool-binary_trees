4231cc8bb2fe96c7dec8e15a4b0ceca19da0c5bd
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-13 20:31:48 -0800

    FEAT: validation in case tree is NULL

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -27,6 +27,11 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	if (!tree)
+	{
+		return (0);
+	}
+
 	if (tree)
 	{
 		size_t l = 0, r = 0;
